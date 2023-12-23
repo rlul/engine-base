ab29ab1165f1f84a485334381d0eb14c2f0c1a26
rlul <45661770+rlul@users.noreply.github.com>
2023-12-24 01:01:14 +0300

    Linux implementation

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -3,16 +3,21 @@
 #else
 
 #endif
+#include <iostream>
 
 #ifdef _WIN32
 extern "C" __declspec(dllexport) int EngineMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 #else
-extern "C" __attribute__((visibility("default"))) int EngineMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+extern "C" __attribute__((visibility("default"))) int EngineMain(int argc, char **argv)
 #endif
 {
 	int result = 0;
 
+    printf("123\n");
+
+#ifdef _WIN32
 	MessageBoxA(0, 0, 0, 0);
+#endif
 
 	return result;
 }
--- launcher/main.cpp
+++ launcher/main.cpp
@@ -1,7 +1,7 @@
 #ifdef _WIN32
 #include <Windows.h>
 #else
-
+#include <dlfcn.h>
 #endif
 #include <string>
 #include <filesystem>
@@ -12,6 +12,7 @@ typedef int (*EngineMain_t)(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR 
 typedef int (*EngineMain_t)(int argc, char **argv);
 #endif
 
+#ifdef _WIN32
 std::string GetBaseDir(std::string_view path)
 {
 	std::filesystem::path base_path(path);
@@ -23,32 +24,50 @@ std::string GetBaseDir(std::string_view path)
 	return base_path.parent_path().generic_string();
 }
 
-#ifdef _WIN32
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	char module_path[MAX_PATH];
 	std::string engine_path;
-	HINSTANCE engine;
+	HINSTANCE engine_module;
 	EngineMain_t engine_main;
 
 	GetModuleFileName(hInstance, module_path, MAX_PATH);
 	engine_path = GetBaseDir(module_path).append("/bin/engine.dll");
-	engine = LoadLibraryEx(engine_path.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
-
-	if (engine == NULL)
+	engine_module = LoadLibraryEx(engine_path.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
+	if (!engine_module)
 	{
-		MessageBoxA(0, "Couldn't load engine library.", "Error", 0);
+		MessageBoxA(0, "Failed to load engine library.", 0, 0);
 		return 0;
 	}
 
-	engine_main = reinterpret_cast<EngineMain_t>(GetProcAddress(engine, "EngineMain"));
+	engine_main = reinterpret_cast<EngineMain_t>(GetProcAddress(engine_module, "EngineMain"));
+    if (!engine_main)
+    {
+        MessageBoxA(0, "Failed to load engine entry point.", 0, 0);
+    }
 
 	return engine_main(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
 }
 #else
 int main(int argc, char** argv)
 {
+    void* engine_module;
+    EngineMain_t engine_main;
 
-	return 0;
+    engine_module = dlopen("bin/libengine.so", RTLD_NOW);
+    if (!engine_module)
+    {
+        printf("Failed to load engine library. (%s)\n", dlerror());
+        return 0;
+    }
+
+    engine_main = (EngineMain_t)dlsym(engine_module, "EngineMain");
+    if (!engine_main)
+    {
+        printf("Failed to load engine entry point. (%s)\n", dlerror());
+        return 0;
+    }
+
+	return engine_main(argc, argv);
 }
 #endif
